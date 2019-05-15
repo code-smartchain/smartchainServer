4c033ca8ffad0a571cf2db22e642e68a2589654c
SpringHawk <martin.zaubitzer@code.berlin>
2019-05-15 16:18:47 +0200

    Adjusted Docker File

--- server/starthApp.sh
+++ server/starthApp.sh
@@ -7,6 +7,6 @@ cd ../hApp
 nohup echo "A)=B&7" | holochain -c ../conductor-config.toml > hApp.log 2>&1 &
 sleep 10
 echo "The new process PID: $( pgrep holochain )"
-echo "Starting Server ..."
-cd ../server
-java -jar server-0.0.1-SNAPSHOT.jar
+#echo "Starting Server ..."
+#cd ../server
+#java -jar server-0.0.1-SNAPSHOT.jar
